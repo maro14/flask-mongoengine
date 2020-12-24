e4742f37fc243ac742b83f1fc2f2a0a7bfa3b509
eki <eki1490@gmail.com>
2020-12-24 19:12:08 +0700

    add post method

--- src/app.py
+++ src/app.py
@@ -4,14 +4,15 @@ from database.db import initialize_db
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
-    'host' : 'mongodb://localhost:27017/customer'
+    'host' : 'mongodb://localhost:27017/customer.user'
 }
 
 initialize_db(app)
 
 @app.route('/')
 def hello():
-    return 'hello'
+    return 'welcome flask-mongoengine'
+
 
 @app.route('/user', methods=["GET"])
 def all_users():
@@ -19,6 +20,13 @@ def all_users():
     return Response(users, mimetype='application/json', status=200)
 
 
+@app.route('/user', methods=["POST"])
+def add_user():
+    body = request.get_json()
+    user = User(**body).save()
+    id = user.id
+    return {'id': str(id)}, 201
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
